38a8efd2886ac57aa484ea9a18bd6f361e078d30
Gabriel Isko <gabestripes@gmail.com>
2018-09-29 15:14:55 -0700

    Modify SubmitCommand Behavior, remove premature optimization.

--- src/RoboteQCommandInterface.py
+++ src/RoboteQCommandInterface.py
@@ -1,5 +1,3 @@
-import serial
-
 #converts roboteq Commands to
 class RoboteqCommander:
 
@@ -9,8 +7,6 @@ class RoboteqCommander:
     QueryDictionary = None
     ConfigDictionary = None
 
-    roboCanNode = None          #Robo Can node command should execute on
-    terminatingCharacter = "_"  #Terminating character commander interface should use for submitting a command to the Roboteq Device
 
     #RoboteqCommander must be constructed by providing a dictionary for all commandString
     #Arguments: _CommandDictionary - dictionary that lists
@@ -18,20 +14,23 @@ class RoboteqCommander:
         #todo Construct commander with corresponding command Dictionaries
         return
 
-    #Function to print command string to Roboteq Device
+    #Function to return command string to Roboteq Device
     #Should be redefined in inherited classes to interface over any port
-    #Aruments: commandString - string that commander should send to RoboteQ Device
+    #Aruments: commandString - string commander should send to RoboteQ Device
+    #Returns: string that will execute on roboteq Device. Should be redefined in derived classes
     def SubmitCommandString(self, commandString):
-        #TODO: print supplied command string and aply robocan node
-        return
+        #TODO: Want to add optimimzations for device specific features
+        #may want to save this functionality for derived classes
+        return commandString
 
     #TODO: catch any error where an invalid token is presented
 
     #submit command using supplied command QueryDictionary
     #TODO: all commanders should have internal dictionaries, so we will have to make sure that this is optimized to pass dictionary.
     def SubmitCommand(self, CommandType, tokenDictionary, token, *args):
-        #TODO: construct string to be be printed by self.submitCommandString
-        return
+        CommandOutput = self.SubmitCommandString(tokenDictionary[token])
+        CommandOutput += " ".join(args)
+        return CommandOutput
 
     #command to call runtime commands
     def setCommand(self, token, *args):
@@ -49,5 +48,3 @@ class RoboteqCommander:
     #function to get configuration settings
     def getConfig(self, token, *args):
         return
-
-class RoboteqSerialCommander(RoboteqCommander):

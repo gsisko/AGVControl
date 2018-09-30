4ed6922275c890526523b4c99b17b054d136198f
gsisko <gabestripes@gmail.com>
2018-09-30 11:35:00 -0700

    Modify tests to check get set method string output

--- tests/commander_tests.py
+++ tests/commander_tests.py
@@ -1,4 +1,5 @@
 import sys, os
+from io import StringIO
 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir) + "\\commanders"))
 
@@ -11,18 +12,31 @@ from RoboteQCommandInterface import RoboteqCommander
 
 class RoboteqCommanderFixture(unittest.TestCase):
 
-
+    TestStreamBuffer = StringIO()
 
     def setUp(self):
-        self.commander = RoboteqCommander(None, None, None)
+        self.commander = RoboteqCommander({'_G':'G', '_M':'M'}, {'_A':'A'}, None, self.TestStreamBuffer)
 
 
 class TestRoboteqCommanderMethods(RoboteqCommanderFixture):
 
     def test_SubmitCommand(self):
-        self.assertEquals(self.commander.SubmitCommand("!",{'_G':'G'},'_G', 1, 300), '!G 1 300')
+        #test sending a normal motor CommandDictionary
+        self.assertEquals(self.commander.CreateCommand("!",{'_G':'G'},'_G', 1, 300), '!G 1 300')
+        #test multiple arguments case
+        self.assertEquals(self.commander.CreateCommand("!",{'_G':'G'},'_G', 1, 300, 300), '!G 1 300 300')
+
+    #test output of Getcommand
+    #TODO: Implement context manager to clear TestStreamBuffer between tests
+
+    def test_getValue(self):
+        self.commander.getValue('_A', 1)
+        self.assertEquals(self.TestStreamBuffer.getvalue().splitlines().pop(), '?A 1')
 
 
+    def test_setCommand(self):
+        self.commander.setCommand('_G', 1, 300)
+        self.assertEquals(self.TestStreamBuffer.getvalue().splitlines().pop(),'!G 1 300')
 
 
 if __name__ == '__main__':

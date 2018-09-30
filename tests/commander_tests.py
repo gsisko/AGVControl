8727ee8e74ec8870e6f433d8a889fcc4e0131fa0
gsisko <gabestripes@gmail.com>
2018-09-30 12:58:37 -0700

    Add mocked readline for test fixture
    Code will have to be refactored in order to context manage a string io 
    input on the fly that responds correctly to output commands that we can 
    test for individually.

--- tests/commander_tests.py
+++ tests/commander_tests.py
@@ -1,18 +1,23 @@
+#system imports to provide proper test execution
 import sys, os
 from io import StringIO
-
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir) + "\\commanders"))
 
+#testing module imports
 import unittest
+from unittest.mock import patch
+from unittest.mock import MagicMock
 
-
+#import Commander for testing
 from RoboteQCommandInterface import RoboteqCommander
 
 
 
 class RoboteqCommanderFixture(unittest.TestCase):
 
+
     TestStreamBuffer = StringIO()
+    TestStreamBuffer.readline = MagicMock(return_value = "-\n")
 
     def setUp(self):
         self.commander = RoboteqCommander({'_G':'G', '_M':'M'}, {'_A':'A'}, None, self.TestStreamBuffer)
@@ -22,21 +27,22 @@ class TestRoboteqCommanderMethods(RoboteqCommanderFixture):
 
     def test_SubmitCommand(self):
         #test sending a normal motor CommandDictionary
-        self.assertEquals(self.commander.CreateCommand("!",{'_G':'G'},'_G', 1, 300), '!G 1 300')
+        self.assertEqual(self.commander.CreateCommand("!",{'_G':'G'},'_G', 1, 300), '!G 1 300')
         #test multiple arguments case
-        self.assertEquals(self.commander.CreateCommand("!",{'_G':'G'},'_G', 1, 300, 300), '!G 1 300 300')
+        self.assertEqual(self.commander.CreateCommand("!",{'_G':'G'},'_G', 1, 300, 300), '!G 1 300 300')
 
     #test output of Getcommand
     #TODO: Implement context manager to clear TestStreamBuffer between tests
 
+
     def test_getValue(self):
-        self.commander.getValue('_A', 1)
-        self.assertEquals(self.TestStreamBuffer.getvalue().splitlines().pop(), '?A 1')
+        self.assertEqual(self.commander.getValue('_A', 1), '-\n')
+        self.assertEqual(self.TestStreamBuffer.getvalue().splitlines().pop(), '?A 1')
 
 
     def test_setCommand(self):
         self.commander.setCommand('_G', 1, 300)
-        self.assertEquals(self.TestStreamBuffer.getvalue().splitlines().pop(),'!G 1 300')
+        self.assertEqual(self.TestStreamBuffer.getvalue().splitlines().pop(),'!G 1 300')
 
 
 if __name__ == '__main__':

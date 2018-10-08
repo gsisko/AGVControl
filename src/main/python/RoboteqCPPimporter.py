df683fddbf618ee9f291621226127b116b3c83c7
gsisko <gabestripes@gmail.com>
2018-10-07 17:27:32 -0700

    Add code that formats string for submission to Roboteq Controller

--- src/main/python/RoboteqCPPimporter.py
+++ src/main/python/RoboteqCPPimporter.py
@@ -18,8 +18,12 @@ print("\n#defines are:")
 for define in cppHeader.defines:
     command = define.split(' ')
     if len(command) == 2:
-        RoboteqCommandDict[command[0]] = command[1] #TODO try to optimize this code to perform proper data access for fast operation.
-
+         #TODO try to optimize this code to perform proper data access for fast operation.
+         #TODO there is definitely a better way to do the string formatting in python
+         #The formatting of the print statement comes from the round about way that values are #defined in the Roboteq CPP install_dependencies
+         #Their identity string is defined as a decimal representation of their HEX valueself.
+         #we need to do all of that conversion here in order to make this work with Roboteq Command Generator.
+        RoboteqCommandDict[command[0]] = '${:02x}'.format(int(command[1])) #using .format() instead of fstring to match formatting in RoboteQ CPP API
 print(RoboteqCommandDict)
 
 ####Defines are formated as keys that will have to be formatted like '${:02X}'.format(value)

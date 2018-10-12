420aad3356971288a2da44ef0ea012a809d70198
gsisko <gabestripes@gmail.com>
2018-10-11 20:11:05 -0700

    Cleaned some comments into docstrings

--- src/main/python/RoboteqSerialCommand.py
+++ src/main/python/RoboteqSerialCommand.py
@@ -7,35 +7,36 @@ import serial
 #Can use this class to mock behavior of RoboteQ command classes in Unittests
 class RoboteqSerialCommander(RoboteqCommand.RoboteqCommandGenerator):
 
-    #RoboteqCommander must be constructed by providing a dictionary for all commandString
-    #Arguments: _CommandDictionary - dictionary that lists all command tokens
-    #           _QueryDictionary - dictionary that lists all query tokens
-    #           _ConfigDictionary - dictionary that lists all config tokens
-    #           _SerialStream - Serial port stream using PySerial that the Commander should interact with.
-    #                           This output stream should be considered as a serial port on a computer
+
     #TODO: create dictionary structure that can check whether supplied command aruments are valid
     def __init__(self, _CommandDictionary, _QueryDictionary, _ConfigDictionary, _SerialStream ):
+        """#RoboteqCommander must be constructed by providing a dictionary for all commandString
+        Arguments: _CommandDictionary - dictionary that lists all command tokens
+                   _QueryDictionary - dictionary that lists all query tokens
+                   _ConfigDictionary - dictionary that lists all config tokens
+                   _SerialStream - Serial port stream using PySerial that the Commander should interact with.
+                                   This output stream should be considered as a serial port on a computer"""
         super().__init__(_CommandDictionary, _QueryDictionary, _ConfigDictionary)
 
         self.outputStream = _SerialStream
 
         return
 
-    #Function decorator to create serial object on the fly using provided settings
     @classmethod
     def connectOverRS232(cls, _CommandDictionary, _QueryDictionary, _ConfigDictionary,*SerialArgs):
+        """Function decorator to create serial object on the fly using provided settings"""
         serialPort = serial.Serial(*SerialArgs, timeout = 1)  #specify default timeout of 1 sec using timeout keyord arg
         Serialcommander1 = cls(_CommandDictionary, _QueryDictionary, _ConfigDictionary, serialPort)
         return Serialcommander1
 
     #TODO make decotorator that just uses default settings to connect over a serial port
 
-    #Function to return command string to Roboteq Device
-    #Should be redefined in inherited classes to interface over any port
-    #Aruments: commandString - string commander should send to RoboteQ Device
-    #Returns: Will retrun a string that is the response from the Roboteq Device
     #TODO Iplement exception raising if there is no response from Roboteq Device, or if somehting else goes wrong with the serial connection.
     def SubmitCommandString(self, commandString):
+        """Function to return command string to Roboteq Device
+        Should be redefined in inherited classes to interface over any port
+        Aruments: commandString - string commander should send to RoboteQ Device
+        Returns: Will retrun a string that is the response from the Roboteq Device"""
         #Submits to user supplied outputStream
         #may want to save this functionality for derived classes
         outputString = commandString + '_'

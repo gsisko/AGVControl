ce8ea0813f3d5ffaf17192990e74817d314d8887
Gabriel Isko <gabestripes@gmail.com>
2018-11-29 16:59:33 -0700

    Add comment

--- src/main/python/RoboteqDevice.py
+++ src/main/python/RoboteqDevice.py
@@ -1,7 +1,6 @@
 
 #Class that encapsulates all RoboteQ Devices
+#It should handle all Comms and Microbasic specific functions for roboteQ devices.
 class RoboteqDevice:
     def __init__(self, _Commander):
         self.Commander = _Commander
-
-    

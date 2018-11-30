b81bfcfdefa5c0f2d06d574658995d6b5e8c22ab
Gabriel Isko <gabestripes@gmail.com>
2018-11-29 17:03:32 -0700

    Add typechecking on RoboteQ Commander Argument

--- src/main/python/RoboteqDevice.py
+++ src/main/python/RoboteqDevice.py
@@ -1,6 +1,13 @@
+from RoboteqCommand import RoboteqCommander
 
-#Class that encapsulates all RoboteQ Devices
-#It should handle all Comms and Microbasic specific functions for roboteQ devices.
+
+.
 class RoboteqDevice:
+    """Class that encapsulates all RoboteQ Devices
+    It should handle all Comms and Microbasic specific functions for roboteQ devices"""
     def __init__(self, _Commander):
-        self.Commander = _Commander
+        """_Commander should be a RoboteQ commander"""
+        if(type(_Commander) == RoboteQCommander)
+            self.Commander = _Commander
+        else
+            except TypeError

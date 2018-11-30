df2fcfe5d745ff8bd04e3dc96fdc38c02045aabe
gsisko <gabestripes@gmail.com>
2018-11-29 19:11:51 -0700

    Add connect method

--- src/main/python/RoboPy.py
+++ src/main/python/RoboPy.py
@@ -1,5 +1,16 @@
-from RoboteqCommand import RoboteqCommand, RoboteqCommander
+from RoboteqCommand import RoboteqCommand, RoboteqCommander, RoboteqCommandLibrary
 from RoboteqSerialCommand import RoboteqSerialCommander
-from RoboteqCPPimporter import RoboteqCPPimporter
+from RoboteqCPPimporter import RoboteqCPPImporter
 
-Connect
+controller = RoboteqCommander(RoboteqCommandLibrary())
+
+#connect to a Device
+def connect():
+    try:
+        tokenList = RoboteqCPPImport.RoboteqImport('Constants.h')
+        controller = RoboteqSerialCommander.connectOverRS232(tokenList)
+    except SerialException:
+        return 'Device not Found'
+    return 'Device Connected'
+
+ 

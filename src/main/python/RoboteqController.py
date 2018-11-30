3bde384559043a00e0694dc721a597edc84c8602
Gabriel Isko <gabestripes@gmail.com>
2018-11-29 17:21:04 -0700

    add controller class

---
+++ src/main/python/RoboteqController.py
@@ -0,0 +1,6 @@
+from RoboteqDevice import RoboteqDevice
+
+class RoboteqController(RoboteqDevice):
+    """Class for all Roboteq Motor controllers
+    should include power monitoring and IO functionality"""
+    def __init__(self)
